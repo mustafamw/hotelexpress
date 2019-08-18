a563189bb665a2a4c683f5ba19b0ec71a0b65dfb
Mustafa Mohamed <mustafa.mohamed@nationalexpress.com>
2019-08-18 16:41:06 +0100

    update

--- routes/user-management/user-management.js
+++ routes/user-management/user-management.js
@@ -1,7 +1,6 @@
 import { router } from '../route/route';
 import { UserDatabase } from '../../services/database/user';
 
-
 /**
  * This function comment is parsed by doctrine
  * @route POST /
@@ -9,12 +8,11 @@ import { UserDatabase } from '../../services/database/user';
  * @returns {object} 200 - An array of user info
  * @returns {Error}  default - Unexpected error
  */
+
 export const create = router.post('/create', (req, res) => {
     const data = req.body;
-    data.created = new Date();
-    
     const userDatabase = new UserDatabase('user');
-    userDatabase.insert([data])
+    userDatabase.insert(data)
     .then((response) => {
         res.send(response);
     })
@@ -23,21 +21,69 @@ export const create = router.post('/create', (req, res) => {
     });
 });
 
-export const get = router.get('/list', (req, res) => {
+export const update = router.put('/update', (req, res) => {
+    const data = req.body;
+    const userDatabase = new UserDatabase('user');
+    userDatabase.update(data)
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((error) => {
+        res.statusCode = error.statusCode;
+        res.send(error);
+    });
+});
+
+export const list = router.get('/list', (req, res) => {
     const userDatabase = new UserDatabase('user');
     userDatabase.find()
     .then((response) => {
         res.send(response);
     })
     .catch((error) => {
+        res.statusCode = error.statusCode;
         res.send(error);
     });
 });
 
-export const update = router.post('/update', (req, res) => {
-    res.send();
+export const listId = router.get('/list/:id', (req, res) => {
+    const userDatabase = new UserDatabase('user');
+    const data = {
+        id: req.params.id
+    }
+    userDatabase.find(data)
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((error) => {
+        res.statusCode = error.statusCode;
+        res.send(error);
+    });
 });
 
-export const remove = router.post('/delete', (req, res) => {
-    res.send();
+export const deleteId = router.delete('/delete/:id', (req, res) => {
+    const userDatabase = new UserDatabase('user');
+    const data = {
+        id: req.params.id
+    }
+    userDatabase.deleteId(data)
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((error) => {
+        res.statusCode = error.statusCode;
+        res.send(error);
+    });
+});
+
+export const drop = router.delete('/delete', (req, res) => {
+    const userDatabase = new UserDatabase('user');
+    userDatabase.delete()
+    .then((response) => {
+        res.send(response);
+    })
+    .catch((error) => {
+        res.statusCode = error.statusCode;
+        res.send(error);
+    });
 });
--- services/database/user.js
+++ services/database/user.js
@@ -1,26 +1,95 @@
 import { MongoDB } from '../../mongodb/mongodb';
+const ObjectId = require('mongodb').ObjectId;
+const HttpStatus = require('http-status-codes');
 export class UserDatabase {
 
-    constructor(collectionName){
+    constructor(collectionName) {
         this.collectionName = collectionName;
         this.dbo = MongoDB.db;
     }
 
-    insert(data){
+    insert(data) {
+        data.created = new Date();
         return new Promise((resolve, reject) => {
-            this.dbo.collection(this.collectionName).insertMany(data, (err, res) => {
-                if (err) reject(err);
-                resolve({message : "Successfully Inserted"});
+            this.dbo.collection(this.collectionName).insertOne(data, (err, res) => {
+                if (err) this.throwError(reject, "Techinal Error", HttpStatus.INTERNAL_SERVER_ERROR);
+                resolve({ message: "Successfully Inserted" });
             });
         });
     }
 
-    find () {
+    update(data) {
         return new Promise((resolve, reject) => {
-            this.dbo.collection(this.collectionName).find().toArray((err, result) => {
-                if (err) reject(err);
+            let query = {};
+            try {
+                query = { "_id": ObjectId(data.id) }
+            } catch (err) {
+                this.throwError(reject, "Incorrect ID", HttpStatus.BAD_REQUEST);
+            }
+            delete data.id;
+            const newvalues = {
+                $set: data
+            }
+            this.dbo.collection(this.collectionName).updateOne(query, newvalues, (err, res) => {
+                if (err) this.throwError(reject, "Techinal Error", HttpStatus.INTERNAL_SERVER_ERROR);
+                resolve({ message: "Successfully Inserted" });
+            });
+        });
+    }
+
+    find(query) {
+        return new Promise((resolve, reject) => {
+            let data = {};
+            try {
+                if (query !== undefined && query !== null && Object.keys(query).length > 0) {
+                    data = { "_id": ObjectId(query.id) }
+                }
+            } catch (err) {
+                this.throwError(reject, "Incorrect ID", HttpStatus.BAD_REQUEST);
+            }
+            this.dbo.collection(this.collectionName).find(data).toArray((err, result) => {
+                if (err) this.throwError(reject, "Technical Error", HttpStatus.INTERNAL_SERVER_ERROR);
                 resolve(result);
             });
         });
+    }
+
+    deleteId(query) {
+        return new Promise((resolve, reject) => {
+            let data = {};
+            try {
+                if (query !== undefined && query !== null && Object.keys(query).length > 0) {
+                    data = { "_id": ObjectId(query.id) }
+                }
+            } catch (err) {
+                this.throwError(reject, "Incorrect ID", HttpStatus.BAD_REQUEST);
+            }
+            this.dbo.collection(this.collectionName).deleteOne(data, (err, obj) => {
+                if (err) this.throwError(reject, "Technical Error", HttpStatus.INTERNAL_SERVER_ERROR);
+                if(obj.deletedCount && obj.deletedCount > 0){
+                    resolve({ message: "Record Deleted" });
+                    return;
+                }
+                this.throwError(reject, "Record Not Found", HttpStatus.NOT_FOUND);
+            });
+        });
+    }
+
+    delete() {
+        return new Promise((resolve, reject) => {
+            this.dbo.collection(this.collectionName).drop((err, obj) => {
+                if (err) this.throwError(reject, "Technical Error", HttpStatus.INTERNAL_SERVER_ERROR);
+                if (obj) resolve({ message: "All Record Deleted" });
+                this.throwError(reject, "Record Not Found", HttpStatus.NOT_FOUND);
+            });
+        });
+    }
+
+    throwError(reject, message, statusCode) {
+        reject({
+            message: message,
+            statusCode: statusCode
+        });
+        return;
     }
 }
