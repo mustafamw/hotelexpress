85c04622d666f8e555f7a4f9dd3909f16cd3f3ae
Mustafa Mohamed <mo.mustafa3@gmail.com>
2019-08-22 23:20:07 +0100

    updated

--- config/config.js
+++ config/config.js
@@ -1,32 +1,32 @@
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 const configs = {
-    development: {
-        application: {
-            name: 'hotelexpress',
-            domain: 'http://localhost:3000',
-            port: 3000
+    'development': {
+        'application': {
+            'name': 'hotelexpress',
+            'domain': 'http://localhost:3000',
+            'port': 3000
         },
-        mongodb: {
-            url: 'mongodb://localhost:27017/hotelexpress'
+        'mongodb': {
+            'url': 'mongodb://localhost:27017/hotelexpress'
         },
-        swagger: {
-            path: '/api-docs'
+        'swagger': {
+            'path': '/api-docs'
         }
     },
-    production: {
-        application: {
-            name: 'hotelexpress',
-            domain: 'http://localhost',
-            port: 80
+    'production': {
+        'application': {
+            'name': 'hotelexpress',
+            'domain': 'http://localhost',
+            'port': 80
         },
-        mongodb: {
-            url: 'mongodb://localhost:27017/hotelexpress'
+        'mongodb': {
+            'url': 'mongodb://localhost:27017/hotelexpress'
         },
-        swagger: {
-            path: '/api-docs'
+        'swagger': {
+            'path': '/api-docs'
         }
     }
 };
 
-export const config = configs[environment];
+export const config = configs[`${environment}`];
